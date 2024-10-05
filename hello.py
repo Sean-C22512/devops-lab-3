80cfc292fd5c89dd713b6fb109f4be63e57efd6b
Sean Cleary <c22512873@ytudublin.ie>
2024-10-05 21:39:21 +0000

    Added /about page to hello.py and updated Flask routes

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<p>Hello, World!</p>'
+
+@app.route('/about')
+def about():
+    return '''
+     <h1>About Page</h1>
+     <p>This is the about page for the Flask app. It gives more information 
+      about the application.</p>
+      '''
